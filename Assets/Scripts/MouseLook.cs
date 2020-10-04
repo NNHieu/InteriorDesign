7898caa59515d1d2abcd83d566531a098df243e2
NNHieu <ngochieutb13@gmail.com>
2020-10-04 20:55:14 +0700

    test commit

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Serializable] private string xInputName;
+    [Serializable] private string yInputName;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void getMouseInput()
+    {
+
+    }
+}
